e8d8d79bde9ecefa5a97e63001e54d6c8a3bfc44
azerpas <xskylix@hotmail.fr>
2020-10-12 17:41:43 +0200

    Init Transformation.java

--- src/main/utils/Transformation.java
+++ src/main/utils/Transformation.java
@@ -1,5 +1,10 @@
 package main.utils;
 
+import main.patchwork.Forme;
+
 public class Transformation {
-    
+    public static void translation(Forme f, final int x, final int y){
+        f.setX(f.getX()+x);
+        f.setY(f.getY()+y);
+    }
 }

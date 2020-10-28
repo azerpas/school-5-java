a510de41a9154af1972afa86ab46fac65195c4f7
azerpas <xskylix@hotmail.fr>
2020-10-28 16:52:44 +0100

    Fixed Homethetie on Polygone and added Translation for Ligne

--- src/main/patchwork/Ligne.java
+++ src/main/patchwork/Ligne.java
@@ -3,7 +3,9 @@ package main.patchwork;
 import java.util.HashSet;
 import java.util.Set;
 
-public class Ligne extends Forme {
+import main.utils.Transformation;
+
+public class Ligne extends Forme implements Transformation {
     private Point pointA;
     private Point pointB;
 
@@ -50,6 +52,37 @@ public class Ligne extends Forme {
         points.add(pointA);
         points.add(pointB);
         return points;
+    }
+
+    @Override
+    public Forme translation(double x, double y) {
+        Point A = new Point(this.getPointA().getX() + x, this.getPointA().getY() + y);
+        Point B = new Point(this.getPointB().getX() + x, this.getPointB().getY() + y);
+        return new Ligne(A,B);
+    }
+
+    @Override
+    public Forme homothetie(double k) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Forme rotation(double angle) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Forme symetrieCentre(Point p) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Forme symetrieAxiale(Point p) {
+        // TODO Auto-generated method stub
+        return null;
     }
 
 }
